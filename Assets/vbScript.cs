830f1d3516dda9ba98519f9e5e9732cd6577208a
Luuk Bouw <luuk@MacBook-Pro-van-Luuk.local>
2019-03-26 11:48:39 +0100

    hallee

---
+++ Assets/vbScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    private GameObject vbButtonObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        vbButtonObject = GameObject.Find("actionButton");
+        vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed (VirtualButtonBehaviour vb) {
+        Debug.Log("onbuttonpressed!");
+    } 
+    public void OnButtonReleased(VirtualButtonBehaviour vb) {
+
+        Debug.Log("niks!");
+    }
+}
